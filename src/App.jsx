47f9b64cbb0cc0a0420bd3f2c4883f3c3e27ac1c
Brunox117 <omarjm0910@gmail.com>
2024-01-29 18:39:29 -0600

    Cambios en el router de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
 import { Footer } from "./components/Footer";
+import { AppRouter } from "./router/AppRouter";
 
 export const App = () => {
   return (
@@ -9,9 +8,7 @@ export const App = () => {
       <h1 className="text-danger">BRUNO BRANCH!!!!!!!!!!!!!</h1>
       <h1>Sustituir esto por un navbar</h1>
       <hr />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <AppRouter />
       <Footer />
     </>
   );
